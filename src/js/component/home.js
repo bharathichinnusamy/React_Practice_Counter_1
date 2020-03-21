771d4a0ab820e5910e5f1ffd05287f0c2e1f7a2b
bharathichinnusamy <lcsbharathi@gmail.com>
2020-03-21 03:04:03 +0000

    trial

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,30 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [state, setState] = useState(0);
+	function addition() {
+		return setState(state + 1);
+	}
+	function substraction() {
+		return setState(state - 1);
+	}
+	function multiplication() {
+		return setState(state * 2);
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div>
+				count:
+				{state}
+			</div>
+			<div>
+				<button onClick={addition}>Add 1</button>
+			</div>
+			<div>
+				<button onClick={substraction}>Minus 1</button>
+			</div>
+			<button onClick={multiplication}>Multiply 2</button>
 		</div>
 	);
 }
